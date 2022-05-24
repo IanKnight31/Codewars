bf5ba54066af5f4354d9b51169534a0f88adfcfb
Ian Knight <ianknightdoyle@gmail.com>
2022-05-23 22:10:25 -0700

    first codewars solution!

--- js/solution001.js
+++ js/solution001.js
@@ -1,1 +1,19 @@
-console.log('solution001')
+//Kata: Multiply, 8kyu
+//Instructions
+//This code does not exceute properly. Try to figure out why.
+
+//Starter Code 
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+//Everything looks good with the spelling and syntax, I believe that it is simply missing a return keyword, lets try.
+
+function multiply(a, b){
+    return a * b
+  }
+
+console.log(multiply(2,3)) //expected 6
+
+//Indeed, 6 displays in the console. 
