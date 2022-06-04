081587f6e659090e74ae6f4f3f4a431348eaea4b
Ian Knight <ianknightdoyle@gmail.com>
2022-06-03 18:26:39 -0700

    Solution14 complete

---
+++ js/solution014.js
@@ -0,0 +1,38 @@
+//Kata: Grasshopper - Messi Goals
+//Instructions: 
+//Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+//Starter Code 
+
+// var laLigaGoals
+// var championsLeagueGoals
+// var copaDelReyGoals
+
+// var totalGoals
+
+//First things first we would probably change the var keyword to the let keyword 
+
+let laLigaGoals = 43
+let championsLeagueGoals = 10 
+let copaDelReyGoals = 5 
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+console.log (totalGoals)
+
+//Indeed we simply add the goals together and store them in totalGoals 
