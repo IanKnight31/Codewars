24002d28dc0d27abb242b16cd3db07e3b6a7e706
Ian Knight <ianknightdoyle@gmail.com>
2022-06-08 21:22:46 -0700

    Solution022 Modified!

---
+++ js/solution022.js
@@ -0,0 +1,12 @@
+//Kata: Function 2 - squaring an argument 
+//Instructions: Now you have to write a function that takes an argument and returns the square of it. 
+
+//No starter code for this one. 
+
+//This is an insanely easy kata, the function is to be named square and it simply has to return the square of whatever number is passed into the argument
+
+function square(n){ 
+    return n**2
+}
+
+//I like the exponent operator in this situation 
