e8159419a13eff2b107b69e0737c75a9269d2623
Ian Knight <ianknightdoyle@gmail.com>
2022-06-21 20:12:18 -0700

    Solution035 Complete!

---
+++ js/solution035.js
@@ -0,0 +1,68 @@
+//Kata: Welcome!, 8kyu 
+//DESCRIPTION:
+// Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+// The Task
+// Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+// The Database
+// english: 'Welcome',
+// czech: 'Vitejte',
+// danish: 'Velkomst',
+// dutch: 'Welkom',
+// estonian: 'Tere tulemast',
+// finnish: 'Tervetuloa',
+// flemish: 'Welgekomen',
+// french: 'Bienvenue',
+// german: 'Willkommen',
+// irish: 'Failte',
+// italian: 'Benvenuto',
+// latvian: 'Gaidits',
+// lithuanian: 'Laukiamas',
+// polish: 'Witamy',
+// spanish: 'Bienvenido',
+// swedish: 'Valkommen',
+// welsh: 'Croeso'
+// Possible invalid inputs include:
+
+// IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+// IP_ADDRESS_NOT_FOUND - ip address not in the database
+// IP_ADDRESS_REQUIRED - no ip address was supplied
+
+//Starter Code 
+
+//I'm going to create my "data base" as an object with literal notation 
+//I'm going to create it inside the function to avoid global variables
+//Then I'm simply going to return the value using my argument for the key and the logic of or to cover it incase the input is not a valid key 
+
+
+function greet(language) {
+	
+    const languageDB = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+        }
+        
+    
+    return languageDB[language]||languageDB['english']
+}
+
+console.log(greet(english))
+
+//This is indeed the top solution on codewars but the top person declared their database globally
+//I admit that i had to look at the solutions because i did not know how to use the logic of or in this case to return a default 
